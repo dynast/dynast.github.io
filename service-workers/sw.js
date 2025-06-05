14bb0c5323a1e7d05c1f9a373ddeb943ccd96c73
Sean King <sean.deking@gmail.com>
2025-06-05 07:26:06 -0600

    Remove outdated fallback comment

--- service-workers/sw.js
+++ service-workers/sw.js
@@ -31,7 +31,7 @@ self.addEventListener('fetch', function (event) {
                 });
                 return response;
             }).catch(function () {
-                /*return caches.match('/sw-test/gallery/myLittleVader.jpg');*/
+                // Optionally return a cached fallback asset here
             });
         }
     }));
